de00542ec18a49ab16893b830e38b743ce669e49
Mukesh Kaushal <imukeshkaushal@gmail.com>
2023-02-26 19:08:37 +0530

    Footer Fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import AllRoutes from './Routers/AllRoutes';
 import Navbar from "./Components/Navbar";
 import { Box } from '@chakra-ui/react';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div>
       <Navbar/>
-      <Box mt={[16,16,16,16,16,24]}>
+      <Box mt={[16,16,16,16,16,24]} >
         <AllRoutes/>
+      </Box>
+      <Box mt={[,4,4,4,16]} borderTop = "1px solid gainsboro" pt = {8}>
+        <Footer/>
       </Box>
     </div>
   );
