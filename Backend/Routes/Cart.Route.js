c8a969d78be4310a1d3db09b2ce5c0891c8351d3
Arpit Kumar Jain <arpitjain2444@gmail.com>
2023-02-27 03:13:28 +0530

    Update Cart.Route.js

--- Backend/Routes/Cart.Route.js
+++ Backend/Routes/Cart.Route.js
@@ -18,10 +18,30 @@ cartRoute.get("/", authenticate, async (req, res) => {
   }
 });
 
+cartRoute.post("/add", authenticate, async (req, res) => {
+  const productId = req.body;
+  let { user } = req.body;
 
+  try {
+    let existingProduct = await CartModel.findOne({ productId, user });
 
-
-
+    if (existingProduct) {
+      let qty = parseFloat(existingProduct.qty + 1);
+      //  let cartItem= await CartModel.findOneAndUpdate({productId,user,qty})
+      let cartItem = new CartModel({ productId, user, qty });
+      await cartItem.save();
+      await CartModel.findOneAndDelete({ productId, user });
+      return res.status(200).send(cartItem);
+    } else {
+      let qty = 1;
+      let cartItem = new CartModel({ productId, user, qty });
+      await cartItem.save();
+      return res.status(200).send(cartItem);
+    }
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+});
 
 
 
