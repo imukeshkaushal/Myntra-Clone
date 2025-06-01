1afe5c0d4f6ba2c1ceebc20ba42217be3e4ffe3c
Arpit Kumar Jain <arpitjain2444@gmail.com>
2025-06-01 23:29:38 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,5 +4,5 @@ const mongoose=require("mongoose")
 require("dotenv").config()
 
 // const connection= mongoose.connect(process.env.mongoURL)
-const connection= mongoose.connect('mongodb+srv://arpitjain2444:<db_password>@cluster0.qxqtwz1.mongodb.net/myntra')
+const connection= mongoose.connect('mongodb+srv://arpitjain2444:arpitjain@cluster0.qxqtwz1.mongodb.net/myntra')
 module.exports={connection}
