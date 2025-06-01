4d832746ef0fca146d3b9617ed1b00e00d2300d3
Arpit Kumar Jain <arpitjain2444@gmail.com>
2025-06-01 23:28:55 +0530

    Update db.js
    checking for db connections , products were not coming

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,6 +3,6 @@ const mongoose=require("mongoose")
 
 require("dotenv").config()
 
-const connection= mongoose.connect(process.env.mongoURL)
-
+// const connection= mongoose.connect(process.env.mongoURL)
+const connection= mongoose.connect('mongodb+srv://arpitjain2444:<db_password>@cluster0.qxqtwz1.mongodb.net/myntra')
 module.exports={connection}
